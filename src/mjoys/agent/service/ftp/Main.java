52280048ee9e27eaf9ad298b8640a5514ac0d500
limeuser <kK644742935@gmail.com>
2015-08-28 14:47:45 +0800

    s

--- src/mjoys/agent/service/ftp/Main.java
+++ src/mjoys/agent/service/ftp/Main.java
@@ -1,10 +1,12 @@
 package mjoys.agent.service.ftp;
 
 import mjoys.util.Address;
+import mjoys.util.Formater;
 import mjoys.util.NumberUtil;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println(String.format("args: %s", Formater.formatArray(args)));
 		FtpServer server = new FtpServer();
 		if (args.length != 3) {
 			System.out.println("please set ftp server port and agent address, for example:\r\n6000 tcp://192.168.1.103:6500");
--- src/mjoys/agent/service/os/Main.java
+++ src/mjoys/agent/service/os/Main.java
@@ -1,9 +1,11 @@
 package mjoys.agent.service.os;
 
 import mjoys.util.Address;
+import mjoys.util.Formater;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println(String.format("args: %s", Formater.formatArray(args)));
 		if (args.length != 2) {
 			System.out.println("please set agent address, for example: tcp://192.168.1.103:6500");
 			return;
