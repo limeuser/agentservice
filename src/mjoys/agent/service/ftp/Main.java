01d1aaa7837333e0d697403091e77f15c98200ad
limeuser <kK644742935@gmail.com>
2015-08-28 15:01:23 +0800

    s

--- src/mjoys/agent/service/ftp/Main.java
+++ src/mjoys/agent/service/ftp/Main.java
@@ -8,13 +8,13 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println(String.format("args: %s", Formater.formatArray(args)));
 		FtpServer server = new FtpServer();
-		if (args.length != 3) {
+		if (args.length != 2) {
 			System.out.println("please set ftp server port and agent address, for example:\r\n6000 tcp://192.168.1.103:6500");
 			return;
 		}
 		
-		Integer ftpPort = NumberUtil.parseInt(args[1]);
-		Address agentAddress = Address.parse(args[2]);
+		Integer ftpPort = NumberUtil.parseInt(args[0]);
+		Address agentAddress = Address.parse(args[1]);
 		if (ftpPort == null || agentAddress == null) {
 			System.out.println("bad paramter, example: 6000 tcp://192.168.1.103:6500");
 			return;
--- src/mjoys/agent/service/os/Main.java
+++ src/mjoys/agent/service/os/Main.java
@@ -6,12 +6,12 @@ import mjoys.util.Formater;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println(String.format("args: %s", Formater.formatArray(args)));
-		if (args.length != 2) {
+		if (args.length != 1) {
 			System.out.println("please set agent address, for example: tcp://192.168.1.103:6500");
 			return;
 		}
 		
-		Address agentAddress = Address.parse(args[1]);
+		Address agentAddress = Address.parse(args[0]);
 		if (agentAddress == null) {
 			System.out.println("bad agent address, for example: tcp://192.168.1.103:6500");
 			return;
